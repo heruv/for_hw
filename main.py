de84edaaf3e6d2b1557865233a9f98597251d79d
heruv <136377622+heruv@users.noreply.github.com>
2023-06-13 16:28:41 +0300

    модуль 4 дз 1
    алгоритм реализован следующим образом: строка, подающаяся на вход, зеркально отражается(rewersed_line) и затем  посимвольно сравнивается со строкой(line), поданной на вход

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def count(line):
-    pass
+def cheker(line):
+  #line = line.replace(" ", "") возможно пригодится
+  #line = line.lower() 
+  rewersed_line = line[:: -1] # разворачиваем строку в другую сторону
+  if line == rewersed_line: # сверяем сторки, посимволно
+      return True
+  else:
+      return False
+      
+  
+   
+
+
