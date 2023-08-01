0b0f7a88fabd7244e699d98660e8c79bcc530f75
heruv <pavelmiragaev12@gmail.com>
2023-08-01 23:02:39 +0300

    work with db

--- online_shop/app_online_shop/models.py
+++ online_shop/app_online_shop/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 #create table-class
 
-class OnlineShop(models.Model): #nasledyem class Moodel
+class OnlineShop(models.Model, ): #nasledyem class Moodel
     #create header
     title = models.CharField('header', max_length=128, ) #CharFiekd class simvolnoe pole 
     #about sell
@@ -15,3 +15,11 @@ class OnlineShop(models.Model): #nasledyem class Moodel
     auction = models.BooleanField('auction?', help_text="mark can you auction pr or?")
     created_time = models.DateTimeField("create time", auto_now_add=True)
     update_time = models.DateTimeField("update time", auto_now=True)
+    
+    
+    def __str__(self):
+        return '%s %s %s' % (f"ID={self.id}" ,self.title, self.prise)
+    
+    
+    class Meta:
+        db_table = 'advertisements'
